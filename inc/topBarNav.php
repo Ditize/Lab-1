e9f883e640b74ded81feea3ff01dfe4330c8619d
Ditize <120607130+Ditize@users.noreply.github.com>
2023-04-14 23:48:45 +0200

    Pjesa-3

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -73,3 +73,24 @@
             </div>
         </nav>
 <script>
+  $(function(){
+   $('#login-btn').click(function(){
+     uni_modal("","login.php")
+   })
+    $('#navbarResponsive').on('show.bs.collapse',function () {
+       $('#mainNav').addClass('navbar-shrink')
+   })
+   $('#navbarResponsive').on('hidden.bs.collapse',function () {
+     if($('body').offset.top ==0)
+     $('#mainNav').removeClass('navbar-shrink')
+  })
+    
+  })
+
+  $('#search-form').submit(function(e){
+    e.preventDefault()
+     var sTxt = $('[name="search"]').val()
+     if(sTxt !='')
+      location.href = './?p=products&serach='+sTxt;
+  })
+  </script>
