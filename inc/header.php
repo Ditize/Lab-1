aa6df2f462ee1e2d60aa18d9d076764187bca2cb
ArionKastrati <ak61269@ubt-uni.net>
2023-04-15 20:34:50 +0200

    krijimi i kompletuar i file-it header.php

--- inc/header.php
+++ inc/header.php
@@ -57,4 +57,24 @@
     <script src="<?php echo base_url ?>dist/js/script.js"></script>
     <script src="<?php echo base_url ?>assets/js/scripts.js"></script>
     <style>
+        #main-header{
+        position:relative;
+        background: rgb(0,0,0)!important;
+        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
+    }
+    #main-header:before{
+        content:"";
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background-image:url(<?php echo base_url.$_settings->info('cover') ?>);
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: drop-shadow(0px 7px 6px black);
+        z-index:-1;
+    }
+
+ </style>
 </head>
