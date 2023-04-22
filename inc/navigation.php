e470ea8e9b1aa0b0cadb4074a51adfc499e46b67
Erijon56101 <en56101@ubt-uni.net>
2023-04-22 11:00:48 +0200

    navigation.php - Part3

--- inc/navigation.php
+++ inc/navigation.php
@@ -99,3 +99,23 @@
         </div>
         <!-- /.sidebar -->
       </aside>
+      <script>
+    $(document).ready(function(){
+      var page = '<?php echo isset($_GET['page']) ? $_GET['page'] : 'home' ?>';
+      var s = '<?php echo isset($_GET['s']) ? $_GET['s'] : '' ?>';
+      if(s!='')
+        page = page+'_'+s;
+      if($('.nav-link.nav-'+page).length > 0){
+             $('.nav-link.nav-'+page).addClass('active')
+        if($('.nav-link.nav-'+page).hasClass('tree-item') == true){
+            $('.nav-link.nav-'+page).closest('.nav-treeview').siblings('a').addClass('active')
+          $('.nav-link.nav-'+page).closest('.nav-treeview').parent().addClass('menu-open')
+        }
+        if($('.nav-link.nav-'+page).hasClass('nav-is-tree') == true){
+          $('.nav-link.nav-'+page).parent().addClass('menu-open')
+        }
+
+      }
+     
+    })
+  </script>
