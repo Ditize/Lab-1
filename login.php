d311328e71ed2cb5bbb3ca6ea2d57a9adca05e6b
ArionKastrati <ak61269@ubt-uni.net>
2023-04-25 02:33:48 +0200

    Krijimi i login.php

--- login.php
+++ login.php
@@ -32,7 +32,7 @@
     </div>
 </div>
 <script>
-     $(function(){
+    $(function(){
         $('#create_account').click(function(){
             uni_modal("","registration.php","mid-large")
         })
@@ -41,8 +41,7 @@
             start_loader()
             if($('.err-msg').length > 0)
                 $('.err-msg').remove();
-
-                $.ajax({
+            $.ajax({
                 url:_base_url_+"classes/Login.php?f=login_user",
                 method:"POST",
                 data:$(this).serialize(),
@@ -51,9 +50,26 @@
                     console.log(err)
                     alert_toast("an error occured",'error')
                     end_loader()
-                }
-                    
                 },
-                })
-                })
-        </script>
+                success:function(resp){
+                    if(typeof resp == 'object' && resp.status == 'success'){
+                        alert_toast("Login Successfully",'success')
+                        setTimeout(function(){
+                            location.reload()
+                        },2000)
+                    }else if(resp.status == 'incorrect'){
+                        var _err_el = $('<div>')
+                            _err_el.addClass("alert alert-danger err-msg").text("Incorrect Credentials.")
+                        $('#login-form').prepend(_err_el)
+                        end_loader()
+                        
+                    }else{
+                        console.log(resp)
+                        alert_toast("an error occured",'error')
+                        end_loader()
+                    }
+                }
+            })
+        })
+    })
+</script>
