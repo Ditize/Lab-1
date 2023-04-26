31b79900ec49cb5c1314cdc867f31d71628ea0a0
ArionKastrati <ak61269@ubt-uni.net>
2023-04-26 12:33:27 +0200

    fillimi i krijimit te registration.php

---
+++ registration.php
@@ -0,0 +1,18 @@
+<style>
+    #uni_modal .modal-content>.modal-footer,#uni_modal .modal-content>.modal-header{
+        display:none;
+    }
+</style>
+<div class="container-fluid">
+    <form action="" id="registration">
+        <div class="row">
+        
+        <h3 class="text-center">Create New Account
+            <span class="float-right">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </span>
+        </h3>
+            <hr>
+        </div>
