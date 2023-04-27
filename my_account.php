6cb5f563d93594f9895589900a290532a44925a5
ArionKastrati <ak61269@ubt-uni.net>
2023-04-27 02:42:47 +0200

    border-warning

---
+++ my_account.php
@@ -0,0 +1,18 @@
+<section class="py-2">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <div class="w-100 justify-content-between d-flex">
+                    <h4><b>Orders</b></h4>
+                    <a href="./?p=edit_account" class="btn btn btn-dark btn-flat"><div class="fa fa-user-cog"></div> Manage Account</a>
+                </div>
+                    <hr class="border-warning">
+                    <table class="table table-stripped text-dark">
+                        <colgroup>
+                            <col width="10%">
+                            <col width="15">
+                            <col width="25">
+                            <col width="25">
+                            <col width="15">
+                        </colgroup>
+ </section>
