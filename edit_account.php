cac97f86c1906e32dbd2496ef1c7e676fb98fe1c
ArionKastrati <ak61269@ubt-uni.net>
2023-04-29 00:31:21 +0200

    edit account.php

--- edit_account.php
+++ edit_account.php
@@ -6,3 +6,26 @@
                     <h4><b>Update Account Details</b></h4>
                     <a href="./?p=my_account" class="btn btn btn-dark btn-flat"><div class="fa fa-angle-left"></div> Back to Order List</a>
                 </div>
+                <hr class="border-warning">
+                    <div class="col-md-6">
+                        <form action="" id="update_account">
+                        <input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
+                            <div class="form-group">
+                                <label for="firstname" class="control-label">Firstname</label>
+                                <input type="text" name="firstname" class="form-control form" value="<?php echo $_settings->userdata('firstname') ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="lastname" class="control-label">Lastname</label>
+                                <input type="text" name="lastname" class="form-control form" value="<?php echo $_settings->userdata('lastname') ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="control-label">Contact</label>
+                                <input type="text" class="form-control form-control-sm form" name="contact" value="<?php echo $_settings->userdata('contact') ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="control-label">Gender</label>
+                                <select name="gender" id="" class="custom-select select" required>
+                                    <option <?php echo $_settings->userdata('gender') == "Male" ? "selected" : '' ?>>Male</option>
+                                    <option <?php echo $_settings->userdata('gender') == "Female" ? "selected" : '' ?>>Female</option>
+                                </select>
+                            </div>
