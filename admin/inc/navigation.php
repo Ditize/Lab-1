32dae416c54d84d1297e43aba99b35368770cd18
Erijon56101 <en56101@ubt-uni.net>
2023-04-29 14:14:18 +0200

    Admin-> navigation.php <-Part1

---
+++ admin/inc/navigation.php
@@ -0,0 +1,96 @@
+</style>
+<!-- Main Sidebar Container -->
+      <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-no-expand">
+        <!-- Brand Logo -->
+        <a href="<?php echo base_url ?>admin" class="brand-link bg-primary text-sm">
+        <img src="<?php echo validate_image($_settings->info('logo'))?>" alt="Store Logo" class="brand-image img-circle elevation-3" style="opacity: .8;width: 2.5rem;height: 2.5rem;max-height: unset">
+        <span class="brand-text font-weight-light"><?php echo $_settings->info('short_name') ?></span>
+        </a>
+        <!-- Sidebar -->
+        <div class="sidebar os-host os-theme-light os-host-overflow os-host-overflow-y os-host-resize-disabled os-host-transition os-host-scrollbar-horizontal-hidden">
+          <div class="os-resize-observer-host observed">
+            <div class="os-resize-observer" style="left: 0px; right: auto;"></div>
+          </div>
+          <div class="os-size-auto-observer observed" style="height: calc(100% + 1px); float: left;">
+            <div class="os-resize-observer"></div>
+          </div>
+          <div class="os-content-glue" style="margin: 0px -8px; width: 249px; height: 646px;"></div>
+          <div class="os-padding">
+            <div class="os-viewport os-viewport-native-scrollbars-invisible" style="overflow-y: scroll;">
+              <div class="os-content" style="padding: 0px 8px; height: 100%; width: 100%;">
+                <!-- Sidebar user panel (optional) -->
+                <div class="clearfix"></div>
+                <!-- Sidebar Menu -->
+                <nav class="mt-4">
+                   <ul class="nav nav-pills nav-sidebar flex-column text-sm nav-compact nav-flat nav-child-indent nav-collapse-hide-child" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item dropdown">
+                      <a href="./" class="nav-link nav-home">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                          Dashboard
+                        </p>
+                      </a>
+                    </li> 
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=product" class="nav-link nav-product">
+                        <i class="nav-icon fas fa-book-open"></i>
+                        <p>
+                          Book List
+                        </p>
+                      </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=inventory" class="nav-link nav-inventory">
+                        <i class="nav-icon fas fa-clipboard-list"></i>
+                        <p>
+                          Inventory List
+                        </p>
+                      </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=orders" class="nav-link nav-orders">
+                        <i class="nav-icon fas fa-list"></i>
+                        <p>
+                          Order List
+                        </p>
+                      </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=sales" class="nav-link nav-sales">
+                        <i class="nav-icon fas fa-file"></i>
+                        <p>
+                          Sales Report
+                        </p>
+                      </a>
+                    </li>
+                    <li class="nav-header">Maintenance</li>
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=maintenance/category" class="nav-link nav-maintenance/category">
+                        <i class="nav-icon fas fa-th-list"></i>
+                        <p>
+                          Category List
+                        </p>
+                      </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=maintenance/sub_category" class="nav-link nav-maintenance/sub_category">
+                        <i class="nav-icon fas fa-th-list"></i>
+                        <p>
+                          Sub Category List
+                        </p>
+                      </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a href="<?php echo base_url ?>admin/?page=system_info" class="nav-link nav-system_info">
+                        <i class="nav-icon fas fa-cogs"></i>
+                        <p>
+                          Settings
+                        </p>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+              </div>
+            </div>
+          </div>
