794c0f9dd02b3975ad76bbf965cd3878013baaee
ArionKastrati <ak61269@ubt-uni.net>
2023-04-30 20:51:25 +0200

    main footer

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -55,3 +55,13 @@
     }
   })
 </script>
+<footer class="main-footer text-sm">
+        <strong>Copyright © <?php echo date('Y') ?>. 
+        <!-- <a href=""></a> -->
+        </strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+          <b><?php echo $_settings->info('short_name') ?> (by: <a href="mailto:oretnom23@gmail.com" target="blank">book23</a> )</b> 
+        </div>
+      </footer>
+    </div>
