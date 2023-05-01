c03a19880ce81aa77a775dd8feee9f8aa941c6d4
Ditize <120607130+Ditize@users.noreply.github.com>
2023-05-01 16:39:06 +0200

    part-1 (Html) crud

---
+++ admin/system_info/index.php
@@ -0,0 +1,57 @@
+<div class="col-lg-12">
+	<div class="card card-outline card-primary">
+		<div class="card-header">
+			<h5 class="card-title">System Information</h5>
+		</div>
+		<div class="card-body">
+			<form action="" id="system-frm">
+				<div id="msg" class="form-group"></div>
+				<div class="form-group">
+					<label for="name" class="control-label">System Name</label>
+					<input type="text" class="form-control form-control-sm" name="name" id="name" value="<?php echo $_settings->info('name') ?>">
+				</div>
+				<div class="form-group">
+					<label for="short_name" class="control-label">System Short Name</label>
+					<input type="text" class="form-control form-control-sm" name="short_name" id="short_name" value="<?php echo  $_settings->info('short_name') ?>">
+				</div>
+			<div class="form-group">
+				<label for="" class="control-label">About Us</label>
+	             <textarea name="about_us" id="" cols="30" rows="2" class="form-control summernote"><?php echo  is_file(base_app.'about.html') ? file_get_contents(base_app.'about.html') : "" ?></textarea>
+			</div>
+			<div class="form-group">
+				<label for="" class="control-label">Privacy Policy</label>
+	             <textarea name="privacy_policy" id="" cols="30" rows="2" class="form-control summernote"><?php echo  is_file(base_app.'privacy_policy.html') ? file_get_contents(base_app.'privacy_policy.html') : "" ?></textarea>
+			</div>
+			
+			<div class="form-group">
+				<label for="" class="control-label">System Logo</label>
+				<div class="custom-file">
+	              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="img" onchange="displayImg(this,$(this))">
+	              <label class="custom-file-label" for="customFile">Choose file</label>
+	            </div>
+			</div>
+			<div class="form-group d-flex justify-content-center">
+				<img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
+			</div>
+			<div class="form-group">
+				<label for="" class="control-label">Website Cover</label>
+				<div class="custom-file">
+	              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="cover" onchange="displayImg2(this,$(this))">
+	              <label class="custom-file-label" for="customFile">Choose file</label>
+	            </div>
+			</div>
+			<div class="form-group d-flex justify-content-center">
+				<img src="<?php echo validate_image($_settings->info('cover')) ?>" alt="" id="cimg2" class="img-fluid img-thumbnail">
+			</div>
+			</form>
+		</div>
+		<div class="card-footer">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-sm btn-primary" form="system-frm">Update</button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
