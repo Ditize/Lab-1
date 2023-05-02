ca8f660ae1c82920dda9bd6858354959a8463088
Erijon56101 <en56101@ubt-uni.net>
2023-05-02 22:20:34 +0200

    Crud (category) - category.php - Admin - Part3

--- admin/maintenance/category.php
+++ admin/maintenance/category.php
@@ -70,3 +70,33 @@
 		</div>
 	</div>
 </div>
+<script>
+	$(document).ready(function(){
+		$('.delete_data').click(function(){
+			_conf("Are you sure to delete this category permanently?","delete_category",[$(this).attr('data-id')])
+		})
+		$('.table').dataTable();
+	})
+	function delete_category($id){
+		start_loader();
+		$.ajax({
+			url:_base_url_+"classes/Master.php?f=delete_category",
+			method:"POST",
+			data:{id: $id},
+			dataType:"json",
+			error:err=>{
+				console.log(err)
+				alert_toast("An error occured.",'error');
+				end_loader();
+			},
+			success:function(resp){
+				if(typeof resp== 'object' && resp.status == 'success'){
+					location.reload();
+				}else{
+					alert_toast("An error occured.",'error');
+					end_loader();
+				}
+			}
+		})
+	}
+</script>
