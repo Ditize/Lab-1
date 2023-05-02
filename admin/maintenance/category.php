e6ccc8520c7c9f707b287cd2e7d36f51adf05b20
Erijon56101 <en56101@ubt-uni.net>
2023-05-02 11:36:09 +0200

    Crud (category) - category.php - Admin - Part1

---
+++ admin/maintenance/category.php
@@ -0,0 +1,41 @@
+<?php if($_settings->chk_flashdata('success')): ?>
+<script>
+	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+</script>
+<?php endif;?>
+<div class="card card-outline card-primary">
+	<div class="card-header">
+		<h3 class="card-title">List of Categories</h3>
+		<div class="card-tools">
+			<a href="?page=maintenance/manage_category" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="container-fluid">
+        <div class="container-fluid">
+			<table class="table table-bordered table-stripped">
+				<colgroup>
+					<col width="5%">
+					<col width="15%">
+					<col width="20%">
+					<col width="35%">
+					<col width="10%">
+					<col width="15%">
+				</colgroup>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Date Created</th>
+						<th>Category</th>
+						<th>Description</th>
+						<th>Status</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$i = 1;
+						$qry = $conn->query("SELECT * from `categories` order by unix_timestamp(date_created) desc ");
+						while($row = $qry->fetch_assoc()):
+                            $row['description'] = strip_tags(stripslashes(html_entity_decode($row['description'])));
+					?>
