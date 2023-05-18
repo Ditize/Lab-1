f386aa28d97d28719f48ef802dcca0267ed6a939
Ditize <120607130+Ditize@users.noreply.github.com>
2023-05-18 17:25:39 +0200

    Part-2 crud=>order

--- admin/orders/index.php
+++ admin/orders/index.php
@@ -1,3 +1,13 @@
+<?php if($_settings->chk_flashdata('success')): ?>
+<script>
+	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+</script>
+<?php endif;?>
+<?php if($_settings->chk_flashdata('error')): ?>
+<script>
+	alert_toast("<?php echo $_settings->flashdata('error') ?>",'error')
+</script>
+<?php endif;?>
 <div class="card card-outline card-primary">
 	<div class="card-header">
 		<h3 class="card-title">List of Orders</h3>
