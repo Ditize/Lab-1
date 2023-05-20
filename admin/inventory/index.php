35d69a6464e62435e3650bb2c4390a461181d117
ArionKastrati <ak61269@ubt-uni.net>
2023-05-20 19:50:09 +0200

    php

--- admin/inventory/index.php
+++ admin/inventory/index.php
@@ -30,3 +30,16 @@
 						<th>Action</th>
 					</tr>
 				</thead>
+				<tbody>
+					<?php 
+					$i = 1;
+						$qry = $conn->query("SELECT i.*,p.title as product,p.author from `inventory` i inner join `products` p on p.id = i.product_id order by unix_timestamp(i.date_created) desc ");
+						while($row = $qry->fetch_assoc()):
+						$sold = $conn->query("SELECT SUM(ol.quantity) as sold FROM order_list ol inner join orders o on o.id = ol.order_id where ol.product_id='{$row['id']}' and o.`status` != 4 ");
+						$sold = $sold->num_rows > 0 ? $sold->fetch_assoc()['sold'] : 0;
+						$avail = $row['quantity'] - $sold;
+						foreach($row as $k=> $v){
+							$row[$k] = trim(stripslashes($v));
+						}
+					?>
+				</tbody>
