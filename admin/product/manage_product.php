ae5f91af78117b8408f0131b83916fea9c96b084
Erijon56101 <en56101@ubt-uni.net>
2023-05-27 23:11:05 +0200

    Crud (Book-List)- manage_product.php -admin  Part1

---
+++ admin/product/manage_product.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_GET['id']) && $_GET['id'] > 0){
+    $qry = $conn->query("SELECT * from `products` where id = '{$_GET['id']}' ");
+    if($qry->num_rows > 0){
+        foreach($qry->fetch_assoc() as $k => $v){
+            $$k=stripslashes($v);
+        }
+    }
+}
+?>
+<div class="card card-outline card-info">
+	<div class="card-header">
+		<h3 class="card-title"><?php echo isset($id) ? "Update ": "Create New " ?> Product</h3>
+	</div>
+	<div class="card-body">
+		<form action="" id="product-form">
+			<input type="hidden" name ="id" value="<?php echo isset($id) ? $id : '' ?>">
+            <div class="form-group">
+				<label for="category_id" class="control-label">Category</label>
+                <select name="category_id" id="category_id" class="custom-select select2" required>
+                <option value=""></option>
+                <?php
+                    $qry = $conn->query("SELECT * FROM `categories` order by category asc");
+                    while($row= $qry->fetch_assoc()):
+                ?>
+                <option value="<?php echo $row['id'] ?>" <?php echo isset($category_id) && $category_id == $row['id'] ? 'selected' : '' ?>><?php echo $row['category'] ?></option>
+                <?php endwhile; ?>
+                </select>
+			</div>
+            <div class="form-group">
+				<label for="sub_category_id" class="control-label">Sub Category</label>
+                <select name="sub_category_id" id="sub_category_id" class="custom-select" required>
+                <option value="" selected="" disabled="">Select Category First</option>
+                <?php
+                    $qry = $conn->query("SELECT * FROM `sub_categories` order by sub_category asc");
+                    $sub_categories = array();
+                    while($row= $qry->fetch_assoc()):
+                    $sub_categories[$row['parent_id']][] = $row;
+                    endwhile; 
+                ?>
+                </select>
+			</div>
