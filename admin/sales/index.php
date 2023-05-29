b9fe50c40b5d146e7b618d8a7fd9d66444f5d0b2
ArionKastrati <ak61269@ubt-uni.net>
2023-05-29 15:33:31 +0200

    date start date end

--- admin/sales/index.php
+++ admin/sales/index.php
@@ -11,3 +11,22 @@ $date_end = isset($_GET['date_end']) ? $_GET['date_end'] :  date("Y-m-d") ;
     <div class="card-header">
         <h5 class="card-title">Sales Report</h5>
     </div>
+    <div class="card-body">
+        <form id="filter-form">
+            <div class="row align-items-end">
+                <div class="form-group col-md-3">
+                    <label for="date_start">Date Start</label>
+                    <input type="date" class="form-control form-control-sm" name="date_start" value="<?php echo date("Y-m-d",strtotime($date_start)) ?>">
+                </div>
+                <div class="form-group col-md-3">
+                    <label for="date_start">Date End</label>
+                    <input type="date" class="form-control form-control-sm" name="date_end" value="<?php echo date("Y-m-d",strtotime($date_end)) ?>">
+                </div>
+                <div class="form-group col-md-1">
+                    <button class="btn btn-flat btn-block btn-primary btn-sm"><i class="fa fa-filter"></i> Filter</button>
+                </div>
+                <div class="form-group col-md-1">
+                    <button class="btn btn-flat btn-block btn-success btn-sm" type="button" id="printBTN"><i class="fa fa-print"></i> Print</button>
+                </div>
+            </div>
+        </form>
