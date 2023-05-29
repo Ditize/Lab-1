ce655bf0c4d162624e3c945596b7a22c10386c35
ArionKastrati <ak61269@ubt-uni.net>
2023-05-29 14:04:10 +0200

    krijimi i index.php te file sales

---
+++ admin/sales/index.php
@@ -0,0 +1,13 @@
+<style>
+    table td,table th{
+        padding: 3px !important;
+    }
+</style>
+<?php 
+$date_start = isset($_GET['date_start']) ? $_GET['date_start'] :  date("Y-m-d",strtotime(date("Y-m-d")." -7 days")) ;
+$date_end = isset($_GET['date_end']) ? $_GET['date_end'] :  date("Y-m-d") ;
+?>
+<div class="card card-primary card-outline">
+    <div class="card-header">
+        <h5 class="card-title">Sales Report</h5>
+    </div>
