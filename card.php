044b82cdbbb1ca20fb9a72cd31d3d500965de3b4
ArionKastrati <ak61269@ubt-uni.net>
2023-06-05 01:36:31 +0200

    php code

--- card.php
+++ card.php
@@ -5,3 +5,22 @@
                 <button class="btn btn-outline-dark btn-flat btn-sm" type="button" id="empty_cart">Empty Cart</button>
             </div>
         </div>
+        <div class="card rounded-0">
+            <div class="card-body">
+                <h3><b>Cart List</b></h3>
+                <hr class="border-dark">
+                <?php 
+                    $qry = $conn->query("SELECT c.*,p.title,i.price,p.id as pid from `cart` c inner join `inventory` i on i.id=c.inventory_id inner join products p on p.id = i.product_id where c.client_id = ".$_settings->userdata('id'));
+                    while($row= $qry->fetch_assoc()):
+                        $upload_path = base_app.'/uploads/product_'.$row['pid'];
+                        $img = "";
+                        foreach($row as $k=> $v){
+                            $row[$k] = trim(stripslashes($v));
+                        }
+                        if(is_dir($upload_path)){
+                            $fileO = scandir($upload_path);
+                            if(isset($fileO[2]))
+                                $img = "uploads/product_".$row['pid']."/".$fileO[2];
+            
+                        }
+                ?>
