ae2dae746672debf5eccb046e84eb9554f31306f
ArionKastrati <ak61269@ubt-uni.net>
2023-06-04 15:46:30 +0200

    fillimi i krijimit te file-it card.php

--- card.php
+++ card.php
@@ -1,1 +1,7 @@
-
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-end mb-2">
+                <button class="btn btn-outline-dark btn-flat btn-sm" type="button" id="empty_cart">Empty Cart</button>
+            </div>
+        </div>
