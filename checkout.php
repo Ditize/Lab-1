cd8c322986d183910145d9c5d0b32737b071a8d2
ArionKastrati <ak61269@ubt-uni.net>
2023-06-08 15:31:18 +0200

    fillimi i file-it checkout.php

--- checkout.php
+++ checkout.php
@@ -1,1 +1,8 @@
-
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+<?php 
+$total = 0;
+    $qry = $conn->query("SELECT c.*,p.title,i.price,p.id as pid from `cart` c inner join `inventory` i on i.id=c.inventory_id inner join products p on p.id = i.product_id where c.client_id = ".$_settings->userdata('id'));
+    while($row= $qry->fetch_assoc()):
+        $total += $row['price'] * $row['quantity'];
+    endwhile;
+?>
