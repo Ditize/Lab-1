c631e097390fa9726d0f72f737786344be8809e8
ArionKastrati <ak61269@ubt-uni.net>
2023-06-08 20:16:56 +0200

    Section

--- checkout.php
+++ checkout.php
@@ -6,3 +6,49 @@ $total = 0;
         $total += $row['price'] * $row['quantity'];
     endwhile;
 ?>
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body"></div>
+            <h3 class="text-center"><b>Checkout</b></h3>
+            <hr class="border-dark">
+            <form action="" id="place_order">
+                <input type="hidden" name="amount" value="<?php echo $total ?>">
+                <input type="hidden" name="payment_method" value="cod">
+                <input type="hidden" name="paid" value="0">
+                <div class="row row-col-1 justify-content-center">
+                    <div class="col-6">
+                    <div class="form-group col mb-0">
+                    <label for="" class="control-label">Order Type</label>
+                    </div>
+                    <div class="form-group d-flex pl-2">
+                        <div class="custom-control custom-radio">
+                          <input class="custom-control-input custom-control-input-primary" type="radio" id="customRadio4" name="order_type" value="2" checked="">
+                          <label for="customRadio4" class="custom-control-label">For Delivery</label>
+                        </div>
+                        <div class="custom-control custom-radio ml-3">
+                          <input class="custom-control-input custom-control-input-primary custom-control-input-outline" type="radio" id="customRadio5" name="order_type" value="1">
+                          <label for="customRadio5" class="custom-control-label">For Pick up</label>
+                        </div>
+                      </div>
+                        <div class="form-group col address-holder">
+                            <label for="" class="control-label">Delivery Address</label>
+                            <textarea id="" cols="30" rows="3" name="delivery_address" class="form-control" style="resize:none"><?php echo $_settings->userdata('default_delivery_address') ?></textarea>
+                        </div>
+                        <div class="col">
+                            <span><h4><b>Total:</b> <?php echo number_format($total) ?></h4></span>
+                        </div>
+                        <hr>
+                        <div class="col my-3">
+                        <h4 class="text-muted">Payment Method</h4>
+                            <div class="d-flex w-100 justify-content-between">
+                                <button class="btn btn-flat btn-dark">Cash on Delivery</button>
+                                <span id="paypal-button"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
