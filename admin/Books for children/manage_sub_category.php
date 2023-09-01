d3e50738c921042a18481b47812b454580408190
Ditize <120607130+Ditize@users.noreply.github.com>
2023-09-01 21:24:26 +0200

    part-1

---
+++ admin/Books for children/manage_sub_category.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_GET['id']) && $_GET['id'] > 0){
+    $qry = $conn->query("SELECT * from `sub_bookCh` where id = '{$_GET['id']}' ");
+    if($qry->num_rows > 0){
+        foreach($qry->fetch_assoc() as $k => $v){
+            $$k=$v;
+        }
+    }
+}
+?>
